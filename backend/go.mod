0a1312f14b2706eaa377f5b71e4d58a179d59010
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2022-12-23 22:56:34 +0100

    dual server setup

---
+++ backend/go.mod
@@ -0,0 +1,7 @@
+module github.com/RRZE-HPC/kerncraft-web
+
+go 1.19
+
+require (
+	github.com/gorilla/mux v1.8.0
+)
--- backend/src/frontendProvider.go
+++ backend/src/frontendProvider.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path"
+
+	"github.com/gorilla/mux"
+)
+
+func serve(addr string, handler http.Handler, name string) {
+	var e error = nil
+	for true {
+		log.Println(name + ": Listening on " + addr)
+		e = http.ListenAndServe(addr, handler)
+		if e != nil {
+			log.Println(name + ": Server on " + addr + " crashed.\n" + e.Error() + "\n" + "Restarting...")
+			e = nil
+		}
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "HOME")
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	ex, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	directory := path.Dir(ex)
+
+	frontendMux := http.NewServeMux()
+	backendMux := http.NewServeMux()
+
+	frontendMux.Handle("/", http.FileServer(http.Dir(directory+"/static")))
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", home)
+	backendMux.Handle("/", router)
+
+	go serve(":"+"3000", frontendMux, "frontend")
+	serve(":"+"3001", backendMux, "backend")
 }
